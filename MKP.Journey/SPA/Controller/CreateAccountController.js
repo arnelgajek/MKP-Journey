50c0553d193430961218ca37645e0aa205ce6f0d
Arnel Gajek <gaar1719@student.ju.se>
2018-04-09 17:54:11 +0200

    Edited: Started working with the CreateAccountController and CreateAccountView. Worked on the ChatHub-layout.

--- MKP.Journey/SPA/Controller/CreateAccountController.js
+++ MKP.Journey/SPA/Controller/CreateAccountController.js
@@ -1,4 +1,18 @@
-﻿app.controller('CreateAccountController', function ($scope) {
+﻿app.controller('CreateAccountController', function ($scope, $http, $route) {
     $scope.titleCreateAccount = "Skapa konto";
+     $scope.createAccountData = {};
 
+    var accountUrl = "http://localhost:53201/api/account/register";
+
+    // Create new account:
+    $scope.createAccount = function () {
+        $http({
+            method: 'POST',
+            url: accountUrl,
+            data: $.param($scope.createAccountData),  // Passes in the data as a string.
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }  // Set the headers so Angular passing info as form data (not request payload)
+        }).then(function (error) {
+            console.log(error);
+        });
+    };
 });
--- MKP.Journey/SPA/SupportChat/ChatHub.cs
+++ MKP.Journey/SPA/SupportChat/ChatHub.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 
@@ -11,6 +12,22 @@ namespace MKP.Journey
         public void Send(string name, string message)
         { 
             Clients.All.broadcastMessage(name, message);
+        }
+
+        public override Task OnConnected()
+        {
+            var connectionId = Context.ConnectionId;
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public override Task OnReconnected()
+        {
+            return base.OnReconnected();
         }
     }
 }
