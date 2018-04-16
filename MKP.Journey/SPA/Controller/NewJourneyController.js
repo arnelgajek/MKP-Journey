f0ff6343144bdd6487b388fdf5a8e52f3bb350d1
Arnel Gajek <gaar1719@student.ju.se>
2018-04-16 10:47:29 +0200

    Edited: The NewJourneyController and NewJourneyView.

--- MKP.Journey/SPA/Controller/NewJourneyController.js
+++ MKP.Journey/SPA/Controller/NewJourneyController.js
@@ -1,3 +1,17 @@
 ﻿app.controller('NewJourneyController', function ($scope) {
     $scope.titleNewJourney = "Registrera ny resa";
+
+    // Create new journey:
+    //$scope.createAccount = function () {
+    //    $http({
+    //        method: 'POST',
+    //        url: accountUrl,
+    //        data: $.param($scope.createAccountData),  // Passes in the data as a string.
+    //        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }  // Set the headers so Angular passing info as form data (not request payload)
+    //    }).then(function (data) { // Sends the user to the Login-page after successfully creating an account.
+    //        $location.url('/');
+    //    }, function (error) {
+    //        console.log(error);
+    //    });
+    //};
 });
