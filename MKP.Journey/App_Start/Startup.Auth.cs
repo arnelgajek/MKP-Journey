2333054916c6daa14430a353ac14bac1445b8ecf
Arnel Gajek <gaar1719@student.ju.se>
2018-05-03 10:33:05 +0200

    Removed: Code from the project that i don't need.

--- MKP.Journey/App_Start/Startup.Auth.cs
+++ MKP.Journey/App_Start/Startup.Auth.cs
@@ -51,25 +51,6 @@ namespace MKP.Journey
 
             // Enable the application to use bearer tokens to authenticate users
             app.UseOAuthBearerTokens(OAuthOptions);
-
-            // Uncomment the following lines to enable logging in with third party login providers
-            //app.UseMicrosoftAccountAuthentication(
-            //    clientId: "",
-            //    clientSecret: "");
-
-            //app.UseTwitterAuthentication(
-            //    consumerKey: "",
-            //    consumerSecret: "");
-
-            //app.UseFacebookAuthentication(
-            //    appId: "",
-            //    appSecret: "");
-
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
         }
     }
 }
