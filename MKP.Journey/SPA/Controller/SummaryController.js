41101fc2e715eacb37c6d4f818409873d3577c4e
Arnel Gajek <gaar1719@student.ju.se>
2018-05-03 10:38:14 +0200

    Removed: Old code from the project.

--- MKP.Journey/SPA/Controller/SummaryController.js
+++ MKP.Journey/SPA/Controller/SummaryController.js
@@ -1,21 +1,7 @@
 ﻿app.controller('SummaryController', function ($scope, $http, $route, $location) {
-    //$scope.titleWelcome = "Welcome";
     $scope.titleOnGoingJourney = "Pågående resa:";
-
-    //var logOutUrl = "http://localhost:53201/api/Account/LogOut";
 
     if (localStorage.getItem("bearer") === null) {
         $location.path('/');
     }
-
-    //$scope.signOutAccount = function () {
-    //    $http({
-    //        method: 'POST',
-    //        url: logOutUrl,
-    //        data: $.param($scope.createAccountData),  // Passes in the data as a string.
-    //        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }  // Set the headers so Angular passing info as form data (not request payload)
-    //    }).then(function (error) {
-    //        console.log(error);
-    //    });
-    //};
 });
